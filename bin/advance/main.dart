805b73c254759ab9600453b1c127ddc9c880af3d
manoj1995madushanka <madushankamanoj414@gmail.com>
2021-01-02 14:55:57 +0530

    working with OS variables

---
+++ bin/advance/main.dart
@@ -0,0 +1,24 @@
+import 'dart:io';
+
+void main(List<String> arguments)
+{
+  getOSVersions();
+}
+
+void getOSVersions()
+{
+  print("OS : ${Platform.operatingSystem} , version ${Platform.version}");
+
+  if(Platform.isWindows)
+    {
+      print("powered by windows");
+    }
+  
+  print(Platform.script.path);
+  print(Platform.executable);
+
+  print("Env. variables");
+  Platform.environment.keys.forEach((element) {
+    print("${element} : ${Platform.environment[element]}");
+  });
+}
