2fddf9eb7768ece6173a1b4d480e48a8290c9c43
manoj1995madushanka <madushankamanoj414@gmail.com>
2021-01-14 17:34:20 +0530

    hashes

---
+++ bin/advance/encryption/hashes.dart
@@ -0,0 +1,18 @@
+import 'dart:convert';
+import 'dart:typed_data';
+
+import 'package:pointycastle/pointycastle.dart';
+
+void main(List<String> arguments)
+{
+  Digest digest = new Digest("SHA-256");
+
+  String value = 'Hello World';
+
+  Uint8List data = new Uint8List.fromList(utf8.encode(value));
+  Uint8List hash = digest.process(data);
+
+  print(hash);
+  print(base64.encode(hash)); // encoding is not encryption
+
+}
